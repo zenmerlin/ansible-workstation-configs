811c800606ac2b10743f2a49ef09bfefd870665d
Jason Boyll <jason.boyll@gmail.com>
2024-07-30 19:43:27 -0400

    add k8s zshrc file

---
+++ roles/k8s/files/k8src.zsh
@@ -0,0 +1,21 @@
+# k8s specific aliases
+
+alias edkconf='vim ~/.kube/config'
+
+alias k='kubectl'
+alias kcx='kubectx'
+
+alias kd='kubectl describe'
+alias kdd='kubectl describe deployment'
+alias kdp='kubectl describe pods'
+alias kds='kubectl describe services'
+
+alias kg='kubectl get'
+alias kga='kubectl get all'
+alias kgd='kubectl get deployments'
+alias kgp='kubectl get pods'
+alias kgs='kubectl get services'
+alias kgsa='kubectl get serviceaccounts'
+
+alias kns='kubens'
+
